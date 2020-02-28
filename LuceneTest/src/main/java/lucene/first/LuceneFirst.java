accb63b7e870a5c8f497c15568a5896fc5ac6ab0
GoTime666 <2811660875@qq.com>
2020-02-28 15:29:51 +0800

    1111

--- LuceneTest/src/main/java/lucene/first/LuceneFirst.java
+++ LuceneTest/src/main/java/lucene/first/LuceneFirst.java
@@ -91,6 +91,7 @@ public class LuceneFirst {
 
 	@Test
 	public void testTokenStream() throws IOException {
+		@SuppressWarnings("resource")
 		Analyzer analyzer = new IKAnalyzer();
 		TokenStream tokenStream = analyzer.tokenStream("", "公安局本以为说好的UTF-16不应该都是2嘛0.0，这里需要科普一下，这个方法的介绍里面有如下这一句话");
 		CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
